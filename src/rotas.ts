49baed5f8caf418ed1f0ae2f3b36072bba5f9191
trickaugusto <trickaugusto1206@gmail.com>
2023-09-20 15:31:29 -0300

    update api

--- src/rotas.ts
+++ src/rotas.ts
@@ -1,9 +1,51 @@
 // rotas.js
-import { Elysia } from 'elysia';
+import { Elysia, t } from 'elysia';
 
-const app = new Elysia() as any;
+const app = new Elysia();
 
-app.get('/visitou-ebd', () => 'visitou-ebd =)');
-app.get('/participou-evento', () => 'participou-evento');
+app.post('/', () => 'pong');
+app.post(
+  '/criar-usuario',
+  ({ body }) => {
+    console.log('Criar usuario:::: ', body);
+    return body;
+  },
+  {
+    body: t.Object({
+      usuario: t.String(),
+      password: t.String(),
+    }),
+  }
+);
+
+app.post(
+  '/visitou-ebd',
+  ({ body }) => {
+    console.log('Visitou-ebd:::: ', body);
+
+    console.log(`${body.mensagemBoasVindas}${body.nomeEbd}`);
+    return body;
+  },
+  {
+    body: t.Object({
+      nomeEbd: t.String(),
+      mensagemBoasVindas: t.String(),
+    }),
+  }
+);
+
+app.post(
+  '/cadastrou-evento',
+  ({ body }) => {
+    console.log('cadastrou-evento:::: ', body);
+    return body;
+  },
+  {
+    body: t.Object({
+      nomeEvento: t.String(),
+      dataEvento: t.String(),
+    }),
+  }
+);
 
 export default app;
